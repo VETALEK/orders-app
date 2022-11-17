205b1a55f5b57f8670a751174601521c81d40ff4
Vitalik Avdyeyenko <dalshe.budet.sobaka@gmail.com>
2022-11-17 17:01:45 +0200

    updated user controller to accept user updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
   def edit
-      @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
-      render 'edit'
+
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :eamil, :role)
   end
 end
