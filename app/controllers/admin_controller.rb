61e582acdd927601bd93c12bbce430382ede3aed
Vitalik Avdyeyenko <dalshe.budet.sobaka@gmail.com>
2022-11-17 13:15:44 +0200

    added user role check to admin_controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,13 @@
 class AdminController < ApplicationController
+  before_action :grant_admins_only_acess
+
+  def grant_admins_only_acess
+    if current_user.role != 'ADMIN'
+      redirect_to root_path,
+      notice: 'Admin rules required to perform this action'
+    end
+  end
+
   def users
     @users = User.all;
   end
