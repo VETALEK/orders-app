4edc2e089f5f280d3b72318ba5ac475cf28f9274
Vitalik Avdyeyenko <dalshe.budet.sobaka@gmail.com>
2022-11-18 15:31:17 +0200

    created item model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :items
   get 'admin/users'
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_085825) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_18_132345) do
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.float "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
