162d3290ebddd4870039ecc714ae6ab99636358f
Vitalik Avdyeyenko <dalshe.budet.sobaka@gmail.com>
2022-11-18 18:04:28 +0200

    allwed only admins to edit items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,9 @@ class ItemsController < ApplicationController
 
   # GET /items/1/edit
   def edit
+    if !@is_user_admin
+      redirect_to admin_items_path
+    end
   end
 
   # POST /items or /items.json
@@ -37,15 +40,15 @@ class ItemsController < ApplicationController
 
   # PATCH/PUT /items/1 or /items/1.json
   def update
-    respond_to do |format|
-      if @item.update(item_params)
-        format.html { redirect_to item_url(@item), notice: "Item was successfully updated." }
-        format.json { render :show, status: :ok, location: @item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    redirect_to admin_items_path
+
+    if !@is_user_admin
+      return
     end
+
+    @item.update(item_params)
+    
+    flash[:notice] = @item.name + ' updated'
   end
 
   # DELETE /items/1 or /items/1.json
